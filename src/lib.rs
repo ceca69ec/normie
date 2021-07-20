c8aa59bad12a91714125b2105c92dd83562f94d2
ceca69ec <86930570+ceca69ec@users.noreply.github.com>
2021-07-20 14:45:11 +0000

    Documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,40 @@
-//! **Rename directories and files into Unix friendly standard.**
+//! **Just another bulk renaming tool.**
+//!
+//! No dependencies, really simple and fast.
+//!
+//! ## Example
+//!
+//! ```shell
+//! $ touch "B)E(T%T@E*R T*H*I&S W@A*Y#" "G)O(O%@D N*A*M&E@**#"
+//! $ ls
+//! 'B)E(T%T@E*R T*H*I&S W@A*Y#'  'G)O(O%@D N*A*M&E@**#'
+//! $ normie -lra .tgz *
+//! $ ls
+//! better_this_way.tgz  good_name.tgz
+//! ```
+//!
+//! ## Help
+//!
+//! ```shell
+//! normie 0.1.0
+//!
+//! USAGE:
+//! 	normie [FLAG]... DIRECTORY_OR_FILE...
+//!
+//! FLAGS:
+//! 	-a: Append the specified text at the end of the filename.
+//! 	-h: Show this help information.
+//! 	-i: Insert the specified text at the beginning of the filename.
+//! 	-l: Transform the resulting filename into all lowercase characters.
+//! 	-r: Remove these characters: '!"#$%&'()*+,/:;<=>?@[\]^`{|}~ªº'.
+//! 	-t: Interactively asks for confirmation of each action.
+//! 	-u: Transform the resulting filename into all uppercase characters.
+//! 	-v: Show information about the performed actions.
+//! ```
+//!
+//! ## Warning
+//!
+//! Use flag `-t` if you are insecure of the results.
 
 use std::env;
 use std::fs;
@@ -135,7 +171,7 @@ fn rename(p: &str, args: &Parsed) -> Result<(), String> {
         }
         let res = format!("{}{}", p.strip_suffix(name).unwrap(), target);
         if args.flg.contains(&'t') && interactive(&args.me, p, &res).is_err() {
-            return Ok(()) 
+            return Ok(())
         }
         match fs::rename(&path, &res[..]) {
             Ok(_) => {
